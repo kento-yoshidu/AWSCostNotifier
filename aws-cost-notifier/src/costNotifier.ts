03cb9deda26bacb9fee94c95e8c7494d3bf064b3
kento <kento@toriwatari.bird>
2023-02-15 21:33:05 +0900

    ✨ Update

--- aws-cost-notifier/src/costNotifier.ts
+++ aws-cost-notifier/src/costNotifier.ts
@@ -1,6 +1,29 @@
 import { IncomingWebhook} from "@slack/webhook"
+import moment from "moment"
+import AWS from "aws-sdk"
 
 export const handler = async (event, context) => {
+  const now = moment()
+  const start = now.format("YYYY-MM-01")
+  const end = now.add(1, "months").format("YYYY-MM-01")
+
+  const ce = new AWS.CostExplorer({ region: "us-east-1" })
+  const params = {
+    TimePeriod: {
+      Start: start,
+      End: end
+    },
+    Granularity: "MONTHLY",
+    Metrics: ["UnblendedCost"]
+  }
+
+  const costAndUsage = await ce.getCostAndUsage(params).promise()
+
+  const usdCost = costAndUsage.ResultsByTime[0].Total.UnblendedCost.Amount
+
+  console.log(usdCost)
+
+  /*
   const slackWebhookUrl = process.env.SLACK_WEBHOOK_URL
   const slackWebhook = new IncomingWebhook(slackWebhookUrl)
 
@@ -9,5 +32,5 @@ export const handler = async (event, context) => {
   return {
     url: slackWebhookUrl
   }
+  */
 }
-
