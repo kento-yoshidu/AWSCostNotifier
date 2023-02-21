d17132d19513b08d448337e8cbcefdcfb75d0940
kento <kento@toriwatari.bird>
2023-02-21 21:39:33 +0900

    ✨ Install SWR

--- todoapp/static/src/App.tsx
+++ todoapp/static/src/App.tsx
@@ -1,19 +1,42 @@
-import React, { useEffect, useState } from 'react';
-
+import React, { useState } from 'react'
+import useSWR from "swr"
 
 type Task = {
   id: string,
   title: string
 }
 
+/* 
+const fetcher = (...args) => fetch(...args).then(res => {
+  res.json()
+})
+*/
+
 function App() {
-  const [task, setTask] = useState("")
+  /*
+  // const { data, error, isLoading } = useSWR(`${process.env.REACT_APP_API_GATEWAY_URL}/tasks`, fetcher)
+
+  const options = {
+    mode: "cors",
+    headers: {
+      
+    }
+  }
+
+  const { data, error, isLoading } = useSWR([`${process.env.REACT_APP_API_GATEWAY_URL}/tasks/`, options], fetcher, {
+    
+  })
+
+  if (error) return <div>error has occurred!</div>
+
+  if (isLoading) return <h1>Loading...</h1>
+
+  if (!isLoading) {
+    console.log({ data })
+  }
+  */
 
   const [taskList, setTaskList] = useState<Task[] | null>(null)
-
-  const handleChange = (e: any) => {
-    setTask(e.target.value)
-  }
 
   const handleClick = async () => {
     const url = `${process.env.REACT_APP_API_GATEWAY_URL}/tasks`
@@ -22,20 +45,12 @@ function App() {
 
     const { tasks } = await result.json()
 
-    console.log(tasks)
-
-    await setTaskList(tasks)
+    setTaskList(tasks)
   }
 
   return (
     <div className="App">
       <form>
-        <h1>Todo App 05</h1>
-
-        <input type="text" onChange={handleChange} />
-
-        <p>{task}</p>
-
         <button onClick={handleClick} type="button">送信</button>
       </form>
 
